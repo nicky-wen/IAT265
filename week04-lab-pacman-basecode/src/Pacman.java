8e5a5d02ac63a46c0fe5c0f6036808bb31fe865f
nicky-wen <starlight0168@gmail.com>
2024-06-03 22:56:13 -0700

    lab completed but doesn't work :l

--- week04-lab-pacman-basecode/src/Pacman.java
+++ week04-lab-pacman-basecode/src/Pacman.java
@@ -3,69 +3,74 @@ import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Panel;
+import java.awt.event.MouseEvent;
 import java.awt.geom.AffineTransform;
 
 import processing.core.PVector;
 
 public class Pacman {
-	
+
 	private int size;
 	private PVector pos, speed;
 	private Color color;
 	private double scale;
 	private boolean selected;
-	
+
 	public Pacman(int x, int y, int size) {
-		
+
 		// given attributes
-		this.pos = new PVector(x,y);
+		this.pos = new PVector(x, y);
 		this.size = size;
-		
+
 		// others
-		this.speed = Util.randomPVector(5);	//random direction of the speed 5 pixels/frame
-		this.color = Util.randomColor();		//random color
-		this.scale = 1;						//default scale = 1
-		this.selected = false;				//not being selected
+		this.speed = Util.randomPVector(5); // random direction of the speed 5 pixels/frame
+		this.color = Util.randomColor(); // random color
+		this.scale = 1; // default scale = 1
+		this.selected = false; // not being selected
 	}
 
 	public void draw(Graphics2D g) {
-		//transformation
-		AffineTransform af = g.getTransform();		
-		g.translate((int)pos.x, (int)pos.y);
+		// transformation
+		AffineTransform af = g.getTransform();
+		g.translate((int) pos.x, (int) pos.y);
 		g.rotate(speed.heading());
 		g.scale(scale, scale);
-		if (speed.x < 0) g.scale(1, -1);
-		
-		//body
+		if (speed.x < 0)
+			g.scale(1, -1);
+
+		// body
 		g.setColor(color);
-		g.fillArc(-size/2, -size/2, size, size, 30, 300);
-		
-		//eye
+		g.fillArc(-size / 2, -size / 2, size, size, 30, 300);
+
+		// eye
 		g.setColor(Color.black);
-		g.fillOval(0, -size/4, size/10, size/10);
-		
+		g.fillOval(0, -size / 4, size / 10, size / 10);
+
 		g.setTransform(af);
-		
-		//bounding box
+
+		// bounding box
 		if (selected) {
 			g.setColor(Color.red);
-			g.drawRect((int)(pos.x-size*scale/2), (int)(pos.y-size*scale/2), (int)(size*scale), (int)(size*scale));
+			g.drawRect((int) (pos.x - size * scale / 2), (int) (pos.y - size * scale / 2), (int) (size * scale),
+					(int) (size * scale));
 		}
 	}
-	
+
 	public void move() {
 		pos.add(speed);
 	}
-	
+
 	public void checkCollision(Dimension panelSize) {
-		if ((pos.x < size/2 * scale) || (pos.x > panelSize.width - size/2 * scale)) speed.x *= -1;
-		if ((pos.y < size/2 * scale) || (pos.y > panelSize.height - size/2 * scale)) speed.y *= -1;
+		if ((pos.x < size / 2 * scale) || (pos.x > panelSize.width - size / 2 * scale))
+			speed.x *= -1;
+		if ((pos.y < size / 2 * scale) || (pos.y > panelSize.height - size / 2 * scale))
+			speed.y *= -1;
 	}
-	
+
 	public void select() {
 		this.selected = true;
 	}
-	
+
 	public void deselect() {
 		this.selected = false;
 	}
@@ -73,17 +78,25 @@ public class Pacman {
 	public void enlarge() {
 		scale *= 1.1;
 	}
-	
+
 	public void shrink() {
 		scale *= .9;
 	}
-	
+
 	public void setScale(double s) {
 		scale = s;
 	}
-	
+
+	public void setPos(int x, int y) {
+		pos.set(x, y);
+	}
+
 	public void setToOtherPacmanScale(Pacman p) {
 		scale = p.scale;
 	}
 
+	public boolean checkMouseHit(MouseEvent e) {
+		return (Math.abs(e.getX() - pos.x) < size / 2) &&
+				(Math.abs(e.getY() - pos.y) < size / 2);
+	}
 }
--- week04-lab-pacman-basecode/src/PacmanPanel.java
+++ week04-lab-pacman-basecode/src/PacmanPanel.java
@@ -4,50 +4,98 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionAdapter;
+import java.util.ArrayList;
 
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
 public class PacmanPanel extends JPanel implements ActionListener {
-	
-	private Pacman p1, p2;
+
+	private class MyMouseMotionAdapter extends MouseMotionAdapter {
+		public void mouseDragged(MouseEvent e) {
+			for (Pacman p : pList) {
+				if (p.checkMouseHit(e))
+					p.setPos(e.getX(), e.getY());
+			}
+		}
+	}
+
+	private class MyMouseAdapter extends MouseAdapter {
+		public void mouseClicked(MouseEvent e) {
+			System.out.println(e.toString());
+		}
+	}
+
+	private ArrayList<Pacman> pList;
 	private Timer t;
 	private Dimension pnlSize;
-	
+
 	public PacmanPanel() {
 		super();
 		this.pnlSize = new Dimension(800, 600);
-		
+
 		this.setPreferredSize(pnlSize);
 
-		this.p1 = new Pacman(pnlSize.width/3, pnlSize.height/2, Math.min(pnlSize.width, pnlSize.height)/10);
-		this.p2 = new Pacman(pnlSize.width/3*2, pnlSize.height/2, Math.min(pnlSize.width, pnlSize.height)/10);
-		
+		this.pList = new ArrayList<Pacman>();
+		for (int i = 0; i < 3; i++) {
+			Pacman p = new Pacman(pnlSize.width / 3, pnlSize.height / 2, Math.min(pnlSize.width, pnlSize.height) / 10);
+			pList.add(p);
+		}
+
 		t = new Timer(33, this);
 		t.start();
+
+		addMouseListener(new MyMouseAdapter());
+		addMouseMotionListener(new MyMouseMotionAdapter());
 	}
-	
+
 	@Override
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		pnlSize = getSize();
 		setBackground(Color.GRAY);
 		Graphics2D g2 = (Graphics2D) g;
-		p1.draw(g2);
-		p2.draw(g2);
+		for (Pacman p : pList) {
+			p.draw(g2);
+		}
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		p1.move();
-//		p1.setScale(p2.scale);
-//		p1.setToOtherPacmanScale(p2);
-		p1.checkCollision(pnlSize);
-		
-		p2.move();
-		p2.checkCollision(pnlSize);
-		
+		for (Pacman p : pList) {
+			p.move();
+		}
+		for (Pacman p : pList) {
+			p.checkCollision(pnlSize);
+		}
+
 		repaint();
+		// p1.setScale(p2.scale);
+		// p1.setToOtherPacmanScale(p2);
 	}
-	
+
+	// public void mouseClicked(MouseEvent e) {
+	// if (p1.checkMouseHit(e))
+	// p1.select();
+	// if (p2.checkMouseHit(e))
+	// p2.select();
+	// }
+
+	public void mousePressed(MouseEvent e) {
+		for (Pacman p : pList)
+			if (p.checkMouseHit(e)) {
+				if (e.isShiftDown())
+					p.enlarge();
+				else if (e.isAltDown())
+					p.shrink();
+				else
+					p.select();
+			} else
+				p.deselect();
+	}
+
 }
